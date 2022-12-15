ab1e4c66063998518cd78994478df29c228c4af8
Richard Gibson <richard@gibson.cx>
2022-12-15 08:12:06 +0000

    F#

---
+++ fsharp/Program.fs
@@ -0,0 +1,15 @@
+﻿
+let playNumber rules number = 
+    rules
+    |> List.choose (fun (divisor, word) -> 
+        if number % divisor = 0 then Some word else None)
+    |> function 
+        | [] -> string number
+        | words -> String.concat "" words
+
+
+let numberGame rules = Seq.map (playNumber rules)
+
+let fizzbuzz = numberGame [ 3, "Fizz"; 5, "Buzz" ]
+
+fizzbuzz {1..100} |> Seq.iter (printfn "%s")
